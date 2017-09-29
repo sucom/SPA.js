56d375fa5d09fd50e77f1aef6982a3facb7f0a43
Kumar <Kumararaja.PanneerSelvam@unisys.com>
2017-09-29 22:17:19 +0530

    v2.7.3 dist file name to .min.js

--- dist/SPAjs/spa.js
+++ dist/SPAjs/spa.js
@@ -79,7 +79,7 @@ var isSpaHashRouteOn=false;
   win.spa = spa;
 
   /* Current version. */
-  spa.VERSION = '2.7.2';
+  spa.VERSION = '2.7.3';
 
   /* isIE or isNonIE */
   var isById = (document.getElementById)
