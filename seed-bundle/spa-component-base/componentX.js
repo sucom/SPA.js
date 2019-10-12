a1d9d5e0cb9ae5df0b3314b6a6a6c9e4b58f2ce2
Sucom <sucom.kumar@gmail.com>
2019-10-13 00:20:07 +0530

    updated with new seed-bundle-base for app and component.

---
+++ seed-bundle/spa-component-base/componentX.js
@@ -0,0 +1,25 @@
+spa.$('componentX', {
+  //style:'.', // Enable only if this component has specific styles in componentX.css
+
+  data: { name: 'componentX' },
+  //dataUrl: '', // use this property for external data source instead of above static data property
+
+  renderCallback: function(){
+    // function called after this component is rendered.
+
+    var templateData = this.$data; //or app.componentX.$data
+    console.log('Component componentX has been rendered with Data:', templateData);
+  },
+
+  doSomething: function(){
+    //call app.componentX.doSomething();
+  },
+
+  events: [
+    // { target: '#btnSubmit',
+    //   onClick: function(){
+    //     app.componentX.doSomething();
+    //   }
+    // }
+  ]
+});
